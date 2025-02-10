8d11cccbe22a1c79842f59cd1a29e36598556b1b
bniladridas <ndas1262000@gmail.com>
2025-02-10 16:27:40 +0530

    fix: Add 'use client' directive to docs page

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,15 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'off',
     'react/no-unescaped-entities': 'off',
     '@next/next/no-img-element': 'off',
-    'jsx-a11y/alt-text': 'off'
+    'jsx-a11y/alt-text': 'off',
+    'no-unused-vars': 'off'
   },
+  ignorePatterns: [
+    '**/*.js',
+    '**/*.jsx',
+    '**/*.ts',
+    '**/*.tsx'
+  ],
   settings: {
     react: {
       version: 'detect'
